aef45d44a906bdd7aa6c92964ed4d817ea8face2
Luis de la Cal <luiscal4a@gmail.com>
2018-01-08 18:34:21 +0100

    Strokes implemented

--- src/paint/Paint.java
+++ src/paint/Paint.java
@@ -18,6 +18,8 @@ package paint;
  */
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -27,6 +29,7 @@ import java.util.logging.Logger;
 import javafx.scene.shape.Line;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import static javax.swing.SwingConstants.CENTER;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -34,6 +37,9 @@ public class Paint implements ActionListener{
 
     private JButton btn_color;
     private JButton btn_stroke,btn_save,btn_background,btn_clear,btn_simmetry;
+    private JButton[] btn_strokes=new JButton[5];
+    private String[] btn_strokesText={"Stroke Size", "Basic Stroke","Wobble Stroke","Composite Stroke", "Dot Stroke"};
+    JComboBox petList;
     public static Color newColor = Color.BLACK;
     public static float newStroke = 6f;
     
@@ -54,17 +60,27 @@ public class Paint implements ActionListener{
         btn_background.addActionListener(this);
         btn_color = new JButton("Color");
         btn_color.addActionListener(this);
-        btn_stroke = new JButton("Stroke");
-        btn_stroke.addActionListener(this);
+        //btn_stroke = new JButton("Stroke");
+        //btn_stroke.addActionListener(this);
         btn_clear = new JButton("Clear");
         btn_clear.addActionListener(this);
         btn_save = new JButton("Save Image");
         btn_save.addActionListener(this);
         btn_simmetry = new JButton("Simmetry factor");
         btn_simmetry.addActionListener(this);
+        
+        for(int i=0;i<btn_strokes.length;i++){
+            btn_strokes[i]=new JButton(btn_strokesText[i]);
+        }
+        
+        petList = new JComboBox(btn_strokesText);
+        petList.setRenderer(new MyComboBoxRenderer("Stroke Options"));
+        petList.setSelectedIndex(-1); 
+        petList.addActionListener(this);
+        
         jpanel.add(btn_background);
         jpanel.add(btn_color);
-        jpanel.add(btn_stroke);
+        jpanel.add(petList);
         jpanel.add(btn_simmetry);
         jpanel.add(btn_clear);
         jpanel.add(btn_save);
@@ -77,7 +93,7 @@ public class Paint implements ActionListener{
         container.add(drawPanel);
         container.add(jpanel);
 
-        JFrame frame = new JFrame("Drawing");
+        JFrame frame = new JFrame("Mandala Maker");
         frame.getContentPane().add(container);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -89,44 +105,71 @@ public class Paint implements ActionListener{
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Paint().createAndShowUI();
+                try {
+                    new Paint().createAndShowUI();
+                     UIManager.setLookAndFeel(
+                            UIManager.getSystemLookAndFeelClassName());
+                } catch (Exception ex) {
+                }
             }
         });
     }
 
-    @Override
+
+    //@Override;
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btn_color)
             newColor = JColorChooser.showDialog(null, "Choose a colour", newColor);
-        else if (e.getSource() == btn_stroke) {
 
-            JOptionPane optionPane = new JOptionPane();
-            JSlider slider = new JSlider();
-            slider.setMajorTickSpacing(10);
-            slider.setPaintTicks(true);
-            slider.setPaintLabels(true);
-            slider.setMaximum(50);
-            slider.setValue(25);
-            optionPane.setInputValue(new Integer(slider.getValue()));
-            ChangeListener changeListener = new ChangeListener() {
-                public void stateChanged(ChangeEvent changeEvent) {
-                    JSlider theSlider = (JSlider) changeEvent.getSource();
-                    if (!theSlider.getValueIsAdjusting()) {
-                        optionPane.setInputValue(new Integer(theSlider.getValue()));
+        else if (e.getSource() == petList) {
+            JComboBox cb = (JComboBox) e.getSource();
+            int int_index = cb.getSelectedIndex();
+            if (int_index == 0) {
+                JOptionPane optionPane = new JOptionPane();
+                JSlider slider = new JSlider();
+                slider.setMajorTickSpacing(10);
+                slider.setPaintTicks(true);
+                slider.setPaintLabels(true);
+                slider.setMaximum(50);
+                slider.setValue(25);
+                optionPane.setInputValue(new Integer(slider.getValue()));
+                ChangeListener changeListener = new ChangeListener() {
+                    public void stateChanged(ChangeEvent changeEvent) {
+                        JSlider theSlider = (JSlider) changeEvent.getSource();
+                        if (!theSlider.getValueIsAdjusting()) {
+                            optionPane.setInputValue(new Integer(theSlider.getValue()));
+                        }
                     }
-                }
-            };
-            slider.addChangeListener(changeListener);
-            optionPane.setMessage(new Object[]{"Select a value: ", slider});
-            optionPane.setMessageType(JOptionPane.QUESTION_MESSAGE);
-            optionPane.setOptionType(JOptionPane.OK_CANCEL_OPTION);
-            JDialog dialog = optionPane.createDialog(null, "My Slider");
-            dialog.setVisible(true);
-            int newStrokeInt=(int) optionPane.getInputValue();
-            newStroke=(float)newStrokeInt;
-            System.out.println("Input: " + newStroke);
-
+                };
+                slider.addChangeListener(changeListener);
+                optionPane.setMessage(new Object[]{"Select a value: ", slider});
+                optionPane.setMessageType(JOptionPane.QUESTION_MESSAGE);
+                optionPane.setOptionType(JOptionPane.OK_CANCEL_OPTION);
+                JDialog dialog = optionPane.createDialog(null, "My Slider");
+                dialog.setVisible(true);
+                int newStrokeInt = (int) optionPane.getInputValue();
+                newStroke = (float) newStrokeInt;
+                System.out.println("Input: " + newStroke);
+               
+                STDrawPanel.strk_strokes[0] = new BasicStroke(Paint.newStroke, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+                STDrawPanel.strk_strokes[1] = new WobbleStroke(5, Paint.newStroke/2);
+                STDrawPanel.strk_strokes[2] = new CompositeStroke(new BasicStroke(Paint.newStroke), new BasicStroke(0.5f));
+                STDrawPanel.strk_strokes[3] = new ShapeStroke(new Shape[]{new Ellipse2D.Float(0, 0, Paint.newStroke, Paint.newStroke)}, Paint.newStroke*4);
+                STDrawPanel.strk_currentStroke = STDrawPanel.strk_strokes[STDrawPanel.int_strokeIndex];
+            } else{
+                STDrawPanel.strk_currentStroke = STDrawPanel.strk_strokes[int_index - 1];
+                STDrawPanel.int_strokeIndex=int_index - 1;
+            }
+                
+            
+            String petName = (String) cb.getSelectedItem();
+            System.out.println(cb.getSelectedIndex());
         }
+        /*else if (e.getSource() == btn_stroke) {
+
+            
+
+        }*/
         
         else if(e.getSource()==btn_save){
             Graphics2D g2 = STDrawPanel.bimg_final.createGraphics();
@@ -155,7 +198,7 @@ public class Paint implements ActionListener{
         }
         
         else if(e.getSource()==btn_simmetry){
-            int ans = Integer.parseInt( JOptionPane.showInputDialog("Select the number of image rows in your layer (10-50)"));
+            int ans = Integer.parseInt( JOptionPane.showInputDialog("Select the simmetry factor(2-25)"));
                 if(ans>=2&&ans<=25){
                     STDrawPanel.int_simmetryFactor=ans;
                     STDrawPanel.arl_line.clear();
@@ -166,7 +209,21 @@ public class Paint implements ActionListener{
     }
     
     public static void saveImage(BufferedImage off_Image){
-        
+         File dir2 = new File("PATH_TO_YOUR_DIRECTORY");
+            
+            JFileChooser chooser = new JFileChooser();
+            chooser.setCurrentDirectory(new java.io.File("."));
+            chooser.showOpenDialog(null);
+            
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                dir2 = new File(chooser.getSelectedFile()+".png");
+            }
+
+            try {
+                ImageIO.write(off_Image, "png", dir2);
+            } catch (IOException ex) {
+                Logger.getLogger(Paint.class.getName()).log(Level.SEVERE, null, ex);
+            }
     }
 
 }
@@ -193,6 +250,20 @@ class STDrawPanel extends JPanel {
     private ArrayList<Point> points = new ArrayList<Point>();
     public static ArrayList<Line> arl_line = new ArrayList<Line>();
     private int colorIndex = 0;
+    public static Stroke[] strk_strokes={new BasicStroke(Paint.newStroke,BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)
+    ,new WobbleStroke(10,10)
+    ,new CompositeStroke( new BasicStroke( 10f ), new BasicStroke( 0.5f ) )
+    ,new ShapeStroke(new Shape[]{new Ellipse2D.Float(0, 0, 4, 4)}, 20.0f)};
+    
+    public static Stroke strk_currentStroke=strk_strokes[0];
+    public static int int_strokeIndex=0;
+            
+            /*g2.setStroke(new BasicStroke(Paint.newStroke,
+            BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));*/
+        /*g2.setStroke(
+		new TextStroke("Hello friend",new Font("Arial", Font.PLAIN, 20),false,false)
+	);*/
+        //g2.setStroke( new CompositeStroke( new BasicStroke( 10f ), new BasicStroke( 0.5f ) ) );
     Point center=null;
 
     public STDrawPanel() {
@@ -242,6 +313,8 @@ class STDrawPanel extends JPanel {
             arl_line.add(new Line(center.x,center.y,point.x,point.y));
         }
     }
+    
+    
     
     private void newPoint(Point point, Point point2, Graphics2D g2){
         /*int int_centerX = this.getWidth() / 2;
@@ -321,8 +394,10 @@ class STDrawPanel extends JPanel {
     private void drawCurve(Graphics2D g2) {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setStroke(new BasicStroke(Paint.newStroke,
-            BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        g2.setStroke(strk_currentStroke);
+        
+                
+        
         g2.setColor(Paint.newColor);
         if (points != null && points.size() > 1) {
             for (int i = 0; i < points.size() - 1; i++) {
@@ -396,5 +471,39 @@ class STMouseAdapter extends MouseAdapter {
     }
 }
 
+ class CompositeStroke implements Stroke {
+		private Stroke stroke1, stroke2;
+
+		public CompositeStroke( Stroke stroke1, Stroke stroke2 ) {
+			this.stroke1 = stroke1;
+			this.stroke2 = stroke2;
+		}
+
+		public Shape createStrokedShape( Shape shape ) {
+			return stroke2.createStrokedShape( stroke1.createStrokedShape( shape ) );
+		}
+	}
+
+
+class MyComboBoxRenderer extends JLabel implements ListCellRenderer {
+  private String _title;
+
+  public MyComboBoxRenderer(String title) {
+    _title = title;
+  }
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value,
+            int index, boolean isSelected, boolean hasFocus) {
+        setHorizontalAlignment(CENTER);
+        setVerticalAlignment(CENTER);
+
+        if (index == -1 && value == null) {
+            setText(_title);
+        } else
+            setText(value.toString());
+        return this;
+    }
+}
 
 
